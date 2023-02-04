07783b68a26be8d5d40194f13eca4d931c0af6f3
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-02-04 22:57:54 +0400

    add pigeon map test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Map, Marker } from "pigeon-maps"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+      <div className='App' id='map'>
+        <Map height={1000} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+          <Marker width={50} anchor={[50.879, 4.6997]} />
+        </Map>
+      </div>
   );
 }
 
