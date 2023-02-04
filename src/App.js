f0fdcac22aeb3bdd0c0513e4b807c2f58b43f392
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-02-04 23:05:52 +0400

    test map size

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Map, Marker } from "pigeon-maps"
 function App() {
     return (
       <div className='App' id='map'>
-        <Map height={1000} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+        <Map height={100} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
           <Marker width={50} anchor={[50.879, 4.6997]} />
         </Map>
       </div>
