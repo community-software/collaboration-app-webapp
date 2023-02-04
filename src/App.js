88ac08c9d63274d7ebbc9abd6b6cb0f9293470c1
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-02-04 23:17:29 +0400

    get height for map from telegram webapp helper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import { Map, Marker } from "pigeon-maps"
+import { useState, useEffect } from 'react';
 
 function App() {
-    return (
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (window.Telegram) {
+      setHeight(window.Telegram.WebApp.viewportHeight);
+    }
+  }, []);
+
+  return (
       <div className='App' id='map'>
-        <Map height={100} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+        <Map height={height} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
           <Marker width={50} anchor={[50.879, 4.6997]} />
         </Map>
       </div>
