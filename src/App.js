42e6da8baf2ddc26b36eccc58ffddb7968b41038
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-05-03 18:30:44 +0400

    replace tile provired

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from 'react';
 import './App.css';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [height, setHeight] = useState(0);
+    const [height, setHeight] = useState(0);
 
-  useEffect(() => {
-    if (window.Telegram) {
-      setHeight(window.Telegram.WebApp.viewportHeight);
-    }
-  }, []);
+    useEffect(() => {
+        if (window.Telegram) {
+            setHeight(window.Telegram.WebApp.viewportHeight);
+        }
+    }, []);
 
-  return (
-      <div className='App' id='map'>
-       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-      </div>
-  );
+    return (
+        <div className='App' id='map'>
+            <MapContainer center={[41.720085105800415, 44.79537545886635]} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+                />
+                <Marker position={[41.720085105800415, 44.79537545886635]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
