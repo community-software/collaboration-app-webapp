ad1333c63134a048a24363a1f2350ffa9d84128d
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-05-03 19:10:16 +0400

    add ui basis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, Tooltip } from 'react-leaflet';
 import { useState, useEffect } from 'react';
 
+function PointDescriptionPopup(props) {
+    const [isOut, setIsOut] = useState(false)
+
+    function handleClose() {
+        setIsOut(true)
+        setTimeout(() => { props.onClose() }, 500)
+    }
+
+    return <div onClick={handleClose} className={`pointPopupWrapper ${isOut && 'out'}`}>
+        <div onClick={e => e.stopPropagation()} className={`pointPopupContent ${isOut && 'out'}`}></div>
+    </div>
+}
 
 function App() {
     const [height, setHeight] = useState(0);
+    const [isPopup, setPopup] = useState(false);
 
     useEffect(() => {
         if (window.Telegram) {
@@ -15,17 +28,21 @@ function App() {
 
     return (
         <div className='App' id='map'>
-            <MapContainer center={[41.720085105800415, 44.79537545886635]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer zoomControl={false} center={[41.720085105800415, 44.79537545886635]} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
                 />
-                <Marker position={[41.720085105800415, 44.79537545886635]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+                <Marker
+                    position={[41.720085105800415, 44.79537545886635]}
+                    eventHandlers={{
+                        click: () => setPopup(true)
+                    }}
+                >
+                    <Tooltip permanent>Tooltip for </Tooltip>
                 </Marker>
             </MapContainer>
+            {isPopup && <PointDescriptionPopup onClose={() => setPopup(false)}></PointDescriptionPopup>}
         </div>
     );
 }
